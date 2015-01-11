28981ba69297b1def53d47c163410a63e9f9e3a8
Marcos Dias <marcos.a.melo.dias@gmail.com>
2015-01-10 23:58:26 -0200

    Controller para o Forge

--- src/main/java/ctrl/ForgeController.java
+++ src/main/java/ctrl/ForgeController.java
@@ -1,7 +1,35 @@
 package ctrl;
 
+import model.ClassDiagram;
+import model.Forge;
+import model.Specification;
+import model.SuperClass;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
- * Created by marcosdias on 09/01/15.
+ * @author Marcos Dias
  */
 public class ForgeController {
+
+    public void createProject(Specification specification) throws IOException {
+        Forge forge = new Forge();
+                        forge.criarProjeto(specification.getName(), "br.edu.leds.ledscode");
+        forge.configBanco(specification.getFeature().getOrm(), specification.getFeature().getDatabase());
+        // TODO Enuns nao foram implementados
+        ClassDiagram diagram = specification.getClassDiagram();
+        ArrayList<SuperClass> classesModel = diagram.getClasses();
+
+        if (specification.getFeature().getApplicationType() == "WEB") {
+            forge.configEntidades(classesModel);
+            forge.gerarInterfaceWeb();
+            forge.configRest();
+            forge.build();
+            forge.exit();
+        }
+
+        forge.print(Forge.ARQUIVO, Forge.EXTENSAO);
+        forge.print(Forge.TERMINAL, null);
+    }
 }
